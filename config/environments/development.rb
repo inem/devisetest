f9d6419da0ffc0522b214543197aed6286131f53
Prelang Builder <builder@prelang.com>
2014-04-25 10:51:38 +0000

    Injected Google Analytics tracker ID configuration into development configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Devisetest::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #->Prelang
+  GA.tracker = "UA-50378289-1"
+
 end
+
